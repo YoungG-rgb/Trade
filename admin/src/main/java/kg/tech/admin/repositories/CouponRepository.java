e7a2cffafb23e51175e81885d692e9e66e60dbe7
Zhalaldin Toichubaev <zhtoichubaev@nurtelecom.kg>
2023-03-13 18:55:40 +0600

    delete back admin module

--- admin/src/main/java/kg/tech/admin/repositories/CouponRepository.java
+++
@@ -1,8 +0,0 @@
-package kg.tech.admin.repositories;
-
-import kg.tech.admin.entities.Coupon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CouponRepository extends JpaRepository<Coupon, Long> {
-
-}
--- admin/src/main/java/kg/tech/admin/repositories/ImageRepository.java
+++
@@ -1,8 +0,0 @@
-package kg.tech.admin.repositories;
-
-import kg.tech.admin.entities.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-
-}
