56a081d01183ebd9d3daa44fda0a40a0c15ea6dc
Zhalaldin Toichubaev <zhtoichubaev@nurtelecom.kg>
2023-03-13 20:38:11 +0600

    add user drop down and header

--- admin/dashboard/components/header.js
+++ admin/dashboard/components/header.js
@@ -1,11 +1,20 @@
-
+import { CgProfile } from 'react-icons/si'
 
 const Header = () => {
     return (
         <header>
             <div>
-
+                <CgProfile />
             </div>
+            <nav>
+                <a>Dashboard</a>
+                <a>Users</a>
+                <a>Items</a>
+                <a>Coupons</a>
+            </nav>
+
         </header>
     )
 }
+
+export default Header;
